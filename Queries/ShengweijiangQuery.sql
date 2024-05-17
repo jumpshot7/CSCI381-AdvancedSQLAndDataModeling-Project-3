abef2df8488e58221316a5135ff12a5804556027
shengweijiang <108643675+ShengweiJiang@users.noreply.github.com>
2024-05-17 00:10:41 -0400

    Add files via upload

---
+++ Queries/ShengweijiangQuery.sql
@@ -0,0 +1,6 @@
+USE QueensClassSchedule
+
+GO
+SELECT BuildingLocation FROM [Location].[BuildingLocation] WHERE BuildingLocation = 'SB'  
+
+SELECT RoomLocation FROM [Location].[BuildingLocation] WHERE BuildingLocation = 'SB'  
